27cea476f2315d3ceaf49bf5052b181d54f5fb07
Jackline Muraya <jacklinewanja.jw@gmail.com>
2023-01-06 15:16:30 +0300

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ * Return: hash value
+**/
+
 {
 	unsigned long int hash;
 	int c;
